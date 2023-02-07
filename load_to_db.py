7e9bf6c783d3b9ba584cdfcd86916cade5cd0c4d
stefanlilja <stefanlilja@brightstraining.com>
2023-02-07 14:22:41 +0100

    database insertion done

--- load_to_db.py
+++ load_to_db.py
@@ -1,19 +1,58 @@
-import sqlalchemy
+from sqlalchemy import Table, Column, String, MetaData, create_engine, TIMESTAMP, Integer
 import pandas as pd
 import os
+from configparser import ConfigParser
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
 data_path = curr_path + '\\data\\'
 
 df = pd.read_csv(data_path + 'cleansed_elephants.csv', sep=',')
 
-engine = sqlalchemy.create_engine('postgresql+psycopg2://postgres:postgres@localhost/elephant')
+config = ConfigParser()
+config.read('database.ini')
+conf = config['postgresql']
 
-con = engine.connect()
+engine = create_engine(f"postgresql://{conf['username']}:{conf['password']}@{conf['hostname']}/{conf['database']}")
 
-insert = con.execute()
+df_time = df[['timestamp', 'year', 'month', 'day', 'hour', 'minute']]
+df_time = df_time.rename(columns={'timestamp': 'time_stamp'})
+df_time.drop_duplicates(inplace=True)
 
-result = con.execute('SELECT * FROM time;')
+df_loc = df[['lat', 'long', 'timestamp', 'external-temperature', 'tag_id']]
+df_loc.rename(
+    columns={
+        'lat': 'latitude',
+        'long': 'longitude',
+        'timestamp': 'time_stamp',
+        'external-temperature': 'temperature'
+    },
+    inplace=True
+)
 
-print(df)
+with engine.connect() as conn:
+    df_time.to_sql('time', conn, if_exists='append', index=False)
+    df_loc.to_sql('location_ping', conn, if_exists='append', index=False)
+
+# meta = MetaData(engine)  
+# time_table = Table('time', meta,  
+#                        Column('time_stamp', TIMESTAMP),
+#                        Column('year', Integer),
+#                        Column('month', Integer),
+#                        Column('day', Integer),
+#                        Column('hour', Integer),
+#                        Column('minute', Integer)
+# )
+
+
+# with engine.connect() as conn:
+#     for i in range(len(df)):
+#         insert_statement = time_table.insert().values(
+#             time_stamp=df.timestamp[i],
+#             year=df.year[i],
+#             month=df.month[i],
+#             day=df.day[i],
+#             hour=df.hour[i],
+#             minute=df.minute[i]
+#             )
+#         conn.execute(insert_statement)
 
