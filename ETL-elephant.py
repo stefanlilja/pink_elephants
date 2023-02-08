579c9ddad975e0bd41d27e77c2b043d5f28c4b5a
BerillVarga <berill.varga@gmail.com>
2023-02-08 15:23:03 +0100

    documented code

--- ETL-elephant.py
+++ ETL-elephant.py
@@ -1,33 +1,152 @@
+# Imports
 import pandas as pd
-import os
-
-curr_path = os.path.dirname(os.path.realpath(__file__))
-work_dir = curr_path + '/data/'
-
-df = pd.read_csv(work_dir + 'ThermochronTracking Elephants Kruger 2007.csv')
-
-df = df.rename(
-    columns={'tag-local-identifier': 'tag_id',
-             'location-lat': 'lat',
-             'location-long': 'long',
-             })
-
-df_new = df.copy()
-df_new[['date', 'time']] = df_new.timestamp.str.split(" ", expand=True)
-df_new[['year', 'month', 'day']] = df_new.date.str.split("-", expand=True)
-df_new[['hour', 'minute']] = df_new.time.str.split(":", expand=True).iloc[:, [0, 1]]
+from load_to_db import data_file_dir
 
 
-df_db = df_new.drop(
-    columns=['event-id',
-             'visible',
-             'sensor-type',
-             'individual-local-identifier',
-             'individual-taxon-canonical-name',
-             'study-name',
-             'date',
-             'time'])
+
+# Function definitions
+def read_raw_data_from_csv(work_dir):
+    """
+    Read raw data from CSV file 
+
+    Parameters
+    ----------
+    work_dir : str
+        File path to the CSV data file.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame with all data from the file.
+
+    """
+    df = pd.read_csv(work_dir + 'ThermochronTracking Elephants Kruger 2007.csv')
+
+    return df
 
 
-df_db.to_csv(work_dir + 'cleansed_elephants.csv')
 
+
+
+def rename_columns(df):
+    """
+    Rename columns in the DataFrame
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame with data.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame with data where columns are named as the columns in the database.
+
+    """
+    df = df.rename(columns = {'tag-local-identifier': 'tag_id',
+                            'location-lat': 'lat',
+                            'location-long': 'long'})
+    
+    return df
+
+
+
+
+
+def split_timestamp(df):
+    """
+    Split timestamp values in the 'timestamp' column to 'year', 'month', 'day', 'hour', 'minute'
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame with data.
+
+    Returns
+    -------
+    df_new : pandas.DataFrame
+        DataFrame with timestamp column splitted into separate columns for year, month, day, hour, minute.
+
+    """
+    df_new = df.copy()
+    df_new[['date', 'time']] = df_new.timestamp.str.split(" ", expand = True)
+    df_new[['year', 'month', 'day']] = df_new.date.str.split("-", expand = True)
+    df_new[['hour', 'minute']] = df_new.time.str.split(":", expand = True).iloc[:, [0, 1]]
+
+    return df_new
+
+
+
+
+
+def clean_data(df_new):
+    """
+    Clean dataframe from unnecessary columns
+
+    Parameters
+    ----------
+    df_new : pandas.DataFrame
+        DataFrame with data.
+
+    Returns
+    -------
+    df_db : pandas.DataFrame
+        DataFrame cleanded and ready for insertion into database.
+
+    """
+    df_db = df_new.drop(columns = ['event-id',
+                                   'visible',
+                                   'sensor-type',
+                                   'individual-local-identifier',
+                                   'individual-taxon-canonical-name',
+                                   'study-name',
+                                   'date',
+                                   'time'])
+
+    return df_db
+
+
+
+
+
+def save_df_to_csv(df_db, work_dir, file_name):
+    """
+    Save cleanded data to CSV file
+
+    Parameters
+    ----------
+    df_db : pandas.DataFrame
+        DataFrame cleanded and ready for insertion into database.
+    work_dir : str
+        File path to the CSV data file.
+    file_name : str
+        Name of the file to save data to (should contain '.csv' at the end).
+
+    Returns
+    -------
+    None.
+
+    """
+    df_db.to_csv(work_dir + file_name)
+
+
+
+
+def main():
+    """
+    Main funtion / programmet
+    """
+    work_dir = data_file_dir()
+    df = read_raw_data_from_csv(work_dir)
+    df = rename_columns(df)
+    df_new = split_timestamp(df)
+    df_db = clean_data(df_new)
+    save_df_to_csv(df_db, work_dir, 'cleansed_elephants.csv')
+
+
+
+
+
+# Main program
+if __name__ == '__main__':
+    main()
--- load_to_db.py
+++ load_to_db.py
@@ -1,58 +1,199 @@
-from sqlalchemy import Table, Column, String, MetaData, create_engine, TIMESTAMP, Integer
+# Imports
+from sqlalchemy import create_engine
 import pandas as pd
 import os
 from configparser import ConfigParser
 
-curr_path = os.path.dirname(os.path.realpath(__file__))
-data_path = curr_path + '\\data\\'
-
-df = pd.read_csv(data_path + 'cleansed_elephants.csv', sep=',')
-
-config = ConfigParser()
-config.read('database.ini')
-conf = config['postgresql']
-
-engine = create_engine(f"postgresql://{conf['username']}:{conf['password']}@{conf['hostname']}/{conf['database']}")
-
-df_time = df[['timestamp', 'year', 'month', 'day', 'hour', 'minute']]
-df_time = df_time.rename(columns={'timestamp': 'time_stamp'})
-df_time.drop_duplicates(inplace=True)
-
-df_loc = df[['lat', 'long', 'timestamp', 'external-temperature', 'tag_id']]
-df_loc.rename(
-    columns={
-        'lat': 'latitude',
-        'long': 'longitude',
-        'timestamp': 'time_stamp',
-        'external-temperature': 'temperature'
-    },
-    inplace=True
-)
-
-with engine.connect() as conn:
-    df_time.to_sql('time', conn, if_exists='append', index=False)
-    df_loc.to_sql('location_ping', conn, if_exists='append', index=False)
-
-# meta = MetaData(engine)  
-# time_table = Table('time', meta,  
-#                        Column('time_stamp', TIMESTAMP),
-#                        Column('year', Integer),
-#                        Column('month', Integer),
-#                        Column('day', Integer),
-#                        Column('hour', Integer),
-#                        Column('minute', Integer)
-# )
 
 
-# with engine.connect() as conn:
-#     for i in range(len(df)):
-#         insert_statement = time_table.insert().values(
-#             time_stamp=df.timestamp[i],
-#             year=df.year[i],
-#             month=df.month[i],
-#             day=df.day[i],
-#             hour=df.hour[i],
-#             minute=df.minute[i]
-#             )
-#         conn.execute(insert_statement)
+# Function definitions
+def data_file_dir():
+    """
+    Get file directory for data file
+
+    Returns
+    -------
+    data_path : str
+        File path to data file.
+
+    """
+    curr_path = os.path.dirname(os.path.realpath(__file__))
+    data_path = curr_path + '\\data\\'
+    
+    return data_path
+
+
+
+
+
+def read_data_from_csv(data_path):
+    """
+    Read data from file to pandas.DataFrame object
+
+    Parameters
+    ----------
+    data_path : str
+        File path to the CSV data file.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        DataFrame with all data from the file.
+
+    """
+    df = pd.read_csv(data_path + 'cleansed_elephants.csv', sep = ',')
+
+    return df
+
+
+
+
+
+def db_configuration(ini_file, section):
+    """
+    Configuration to the database
+
+    Parameters
+    ----------
+    ini_file : str
+        File name of ini-file with configuration data.
+    section : str
+        Section of the ini-file to look into.
+
+    Returns
+    -------
+    conf : configparser.SectionProxy
+        Configuration parser object to connect to the database.
+
+    """
+    config = ConfigParser()
+    config.read(ini_file)
+    conf = config[section]
+
+    return conf
+
+
+
+
+
+def create_engine_for_data(conf):
+    """
+    Creat engine for the data
+
+    Parameters
+    ----------
+    conf : configparser.SectionProxy
+        Configuration parser object to connect to the database.
+
+    Returns
+    -------
+    engine : sqlalchemy.engine.base.Engine
+        Engine object for connection with database.
+
+    """
+    engine = create_engine(f"postgresql://{conf['username']}:{conf['password']}@{conf['hostname']}/{conf['database']}")
+
+    return engine
+
+
+
+
+
+def create_clean_time_dataset(df):
+    """
+    Clean time data for insertion into the time table in the database 
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Raw dataset in pandas.DataFrame.
+
+    Returns
+    -------
+    df_time : pandas.DataFrame
+        Clean time data in pandas.DataFrame.
+
+    """
+    df_time = df[['timestamp', 'year', 'month', 'day', 'hour', 'minute']]
+    df_time = df_time.rename(columns = {'timestamp': 'time_stamp'})
+    df_time = df_time.drop_duplicates()
+
+    return df_time
+
+
+
+
+
+def create_clean_location_dataset(df):
+    """
+    Clean location data for insertion into the location table in the database  
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        Raw dataset in pandas.DataFrame.
+
+    Returns
+    -------
+    df_loc : pandas.DataFrame
+        Clean location data in pandas.DataFrame.
+
+    """
+    df_loc = df[['lat', 'long', 'timestamp', 'external-temperature', 'tag_id']]
+    df_loc = df_loc.rename(columns = {'lat': 'latitude',
+                                      'long': 'longitude',
+                                      'timestamp': 'time_stamp',
+                                      'external-temperature': 'temperature'})
+
+    return df_loc
+
+
+
+
+
+def insert_into_db(engine, df_time, df_loc):
+    """
+    Insertion of data into database tables (time and location)
+
+    Parameters
+    ----------
+    engine : sqlalchemy.engine.base.Engine
+        Engine object for connection with database.
+    df_time : pandas.DataFrame
+        Clean time data in pandas.DataFrame.
+    df_loc : pandas.DataFrame
+        Clean location data in pandas.DataFrame.
+
+    Returns
+    -------
+    None.
+
+    """
+    with engine.connect() as conn:
+        df_time.to_sql('time', conn, if_exists = 'append', index = False)
+        df_loc.to_sql('location_ping', conn, if_exists = 'append', index = False)
+
+
+
+
+
+def main():
+    """
+    Main funtion / programmet
+    """
+    data_path = data_file_dir()
+    df = read_data_from_csv(data_path)
+    conf = db_configuration('database.ini', 'postgresql')
+    engine = create_engine_for_data(conf)
+    df_time = create_clean_time_dataset(df)
+    df_loc = create_clean_location_dataset(df)
+    insert_into_db(engine, df_time, df_loc)
+    
+
+
+
+
+# Main program
+if __name__ == '__main__':
+    main()
 
